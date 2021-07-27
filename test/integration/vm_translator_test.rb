f527a53a6d08a3eba43921390201816b0cc65d8e
wildmaples <maple.ong@shopify.com>
2021-07-27 12:48:18 -0400

    Move assembly code into fixtures files

--- test/integration/vm_translator_test.rb
+++ test/integration/vm_translator_test.rb
@@ -3,83 +3,19 @@ require "test_helper"
 class VMTranslatorIntegrationTest < Minitest::Test
   def test_integration_test
     assembly_code = `bin/vm-translator examples/Push.vm`
-    expected = <<~EOF
-      @17
-      D=A
-      @SP
-      A=M
-      M=D
-      @SP
-      M=M+1
-    EOF
-
+    expected = File.read("test/fixtures/Push.asm")
     assert_equal(expected, assembly_code)
   end
 
   def test_integration_test_simple_add
     assembly_code = `bin/vm-translator examples/SimpleAdd.vm`
-    expected = <<~EOF
-      @7
-      D=A
-      @SP
-      A=M
-      M=D
-      @SP
-      M=M+1
-      @8
-      D=A
-      @SP
-      A=M
-      M=D
-      @SP
-      M=M+1
-      AM=M-1
-      D=M
-      A=A-1
-      M=M+D
-    EOF
-
+    expected = File.read("test/fixtures/SimpleAdd.asm")
     assert_equal(expected, assembly_code)
   end
 
   def test_integration_test_simple_eq
     assembly_code = `bin/vm-translator examples/SimpleEq.vm`
-    expected = <<~EOF
-      @8
-      D=A
-      @SP
-      A=M
-      M=D
-      @SP
-      M=M+1
-      @9
-      D=A
-      @SP
-      A=M
-      M=D
-      @SP
-      M=M+1
-      AM=M-1
-      D=M
-      A=A-1
-      
-      D=M-D
-      @EQUAL
-      D;JEQ
-      
-      @SP
-      A=M-1
-      M=0
-      @END
-      0;JMP
-      
-      (EQUAL)
-      @SP
-      A=M-1
-      M=-1
-      (END)
-    EOF
-
+    expected = File.read("test/fixtures/SimpleEq.asm")
     assert_equal(expected, assembly_code)
   end
 end
