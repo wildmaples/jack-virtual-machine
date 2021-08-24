743bd09d06188e44067c1b25d08ca946d97576ca
wildmaples <maple.ong@shopify.com>
2021-08-24 07:30:39 -0700

    Hook up :C_POP to vm translator

--- lib/vm_translator.rb
+++ lib/vm_translator.rb
@@ -15,8 +15,8 @@ class VMTranslator
     while parser.has_more_commands?
       parser.advance
       case parser.command_type
-      when :C_PUSH
-        @code_writer.write_push_pop(:C_PUSH, parser.arg1, parser.arg2)
+      when :C_PUSH, :C_POP
+        @code_writer.write_push_pop(parser.command_type, parser.arg1, parser.arg2)
       when :C_ARITHMETIC
         @code_writer.write_arithmetic(parser.arg1)
       end
