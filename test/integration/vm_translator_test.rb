e0d2eda0551713b2e72e821772761fba7366b19a
wildmaples <maple.ong@shopify.com>
2021-09-15 13:07:39 -0400

    Use :base and File#basename

--- test/integration/vm_translator_test.rb
+++ test/integration/vm_translator_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class VMTranslatorIntegrationTest < Minitest::Test
-  Dir.glob("examples/**").each do |file_path|
-    file_name = /examples\/(.*).vm/.match(file_path)[1]
-    test_name = "test_integration_#{file_name}"
+  Dir.glob("*.vm", base: "examples").each do |file_name|
+    base_name = File.basename(file_name, ".*")
+    test_name = "test_integration_#{base_name}"
 
     define_method(test_name) do
-      assembly_code = `bin/vm-translator #{file_path}`
-      expected = File.read("test/fixtures/#{file_name}.asm")
+      assembly_code = `bin/vm-translator examples/#{file_name}`
+      expected = File.read("test/fixtures/#{base_name}.asm")
       assert_equal(expected, assembly_code)
     end
   end
