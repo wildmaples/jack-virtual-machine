1f1b7782f085cb9fbaabc74ca7a0d6299ba9d6be
wildmaples <maple.ong@shopify.com>
2021-09-24 11:32:33 -0400

    Translate :C_LABEL and :C_IF in the VMTranslator

--- lib/vm_translator.rb
+++ lib/vm_translator.rb
@@ -17,6 +17,10 @@ class VMTranslator
       case parser.command_type
       when :C_PUSH, :C_POP
         @code_writer.write_push_pop(parser.command_type, parser.arg1, parser.arg2)
+      when :C_LABEL
+        @code_writer.write_label(parser.arg1)
+      when :C_IF
+        @code_writer.write_if(parser.arg1)
       when :C_ARITHMETIC
         @code_writer.write_arithmetic(parser.arg1)
       end
