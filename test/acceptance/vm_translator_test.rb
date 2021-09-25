95070d96f3187331474df9ba8cc6222ca5b87e10
Tom Stuart <tom.stuart@shopify.com>
2021-09-25 12:47:44 +0100

    Automatically generate an acceptance test for each script
    This copies the technique used for the integration tests in #12: we
    iterate over each of the `test/fixtures/*.tst` files and use
    `define_method` to generate a test for each of them individually.
    
    Now we can add acceptance tests for other `examples/*.vm` files simply
    by copying the appropriate `.tst` and `.cmp` files into `test/fixtures`,
    which we‘ll do in the next commit.

--- test/acceptance/vm_translator_test.rb
+++ test/acceptance/vm_translator_test.rb
@@ -5,42 +5,47 @@ require "tmpdir"
 class VMTranslatorAcceptanceTest < Minitest::Test
   CPU_EMULATOR_PATH = ENV["CPU_EMULATOR"]
 
-  def test_acceptance_test_simple_add
-    skip "can’t find CPU emulator, please set CPU_EMULATOR" unless CPU_EMULATOR_PATH
+  Dir.glob("*.tst", base: "test/fixtures").each do |file_name|
+    base_name = File.basename(file_name, ".*")
+    test_name = "test_acceptance_#{base_name}"
 
-    # make a temporary directory
-    Dir.mktmpdir do |temporary_directory|
-      # copy the SimpleAdd.tst and SimpleAdd.cmp files into it
-      FileUtils.copy(
-        [
-          "test/fixtures/SimpleAdd.tst",
-          "test/fixtures/SimpleAdd.cmp"
-        ],
-        temporary_directory
-      )
+    define_method(test_name) do
+      skip "can’t find CPU emulator, please set CPU_EMULATOR" unless CPU_EMULATOR_PATH
 
-      # translate SimpleAdd.vm and write the result into SimpleAdd.asm
-      assembly_code = `bin/vm-translator examples/SimpleAdd.vm`
-      assembly_code_path = File.join(temporary_directory, "SimpleAdd.asm")
-      File.write(assembly_code_path, assembly_code)
+      # make a temporary directory
+      Dir.mktmpdir do |temporary_directory|
+        # copy the .tst and .cmp files into it
+        FileUtils.copy(
+          [
+            "test/fixtures/#{base_name}.tst",
+            "test/fixtures/#{base_name}.cmp"
+          ],
+          temporary_directory
+        )
 
-      # run SimpleAdd.tst in the CPU emulator and remember its exit status
-      test_script_path = File.join(temporary_directory, "SimpleAdd.tst")
-      cpu_emulator_exit_status = nil
-      _cpu_emulator_output, cpu_emulator_error = capture_subprocess_io do
-        cpu_emulator_exit_status = system(CPU_EMULATOR_PATH, test_script_path)
-      end
+        # translate .vm and write the result into .asm
+        assembly_code = `bin/vm-translator examples/#{base_name}.vm`
+        assembly_code_path = File.join(temporary_directory, "#{base_name}.asm")
+        File.write(assembly_code_path, assembly_code)
 
-      # generate a diff between SimpleAdd.cmp and SimpleAdd.out
-      unless cpu_emulator_exit_status
-        expected_result, actual_result = ["cmp", "out"].map do |extension|
-          File.read(File.join(temporary_directory, "SimpleAdd.#{extension}"))
+        # run .tst in the CPU emulator and remember its exit status
+        test_script_path = File.join(temporary_directory, "#{base_name}.tst")
+        cpu_emulator_exit_status = nil
+        _cpu_emulator_output, cpu_emulator_error = capture_subprocess_io do
+          cpu_emulator_exit_status = system(CPU_EMULATOR_PATH, test_script_path)
         end
-        cpu_emulator_error << diff(expected_result, actual_result)
-      end
 
-      # check that the exit status was `true` (i.e. success)
-      assert(cpu_emulator_exit_status, cpu_emulator_error)
+        # generate a diff between .cmp and .out
+        unless cpu_emulator_exit_status
+          expected_result, actual_result = ["cmp", "out"].map do |extension|
+            File.read(File.join(temporary_directory, "#{base_name}.#{extension}"))
+          end
+          cpu_emulator_error << diff(expected_result, actual_result)
+        end
+
+        # check that the exit status was `true` (i.e. success)
+        assert(cpu_emulator_exit_status, cpu_emulator_error)
+      end
     end
   end
 end
