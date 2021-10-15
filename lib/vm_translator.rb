89ecca67ce08ecf0d51f1507e60dc32e7e22277c
wildmaples <maple.ong@shopify.com>
2021-10-15 15:48:52 -0400

    Remove unnessary instance variables

--- lib/vm_translator.rb
+++ lib/vm_translator.rb
@@ -8,35 +8,35 @@ class VMTranslator
   end
 
   def translate(input_file)
-    @out = StringIO.new
-    @code_writer = CodeWriter.new(@out)
+    out = StringIO.new
+    code_writer = CodeWriter.new(out)
     parser = Parser.new(input_file)
     file_name = File.basename(input_file.path, ".vm")
 
-    @code_writer.set_file_name(file_name)
+    code_writer.set_file_name(file_name)
     while parser.has_more_commands?
       parser.advance
       case parser.command_type
       when :C_PUSH, :C_POP
-        @code_writer.write_push_pop(parser.command_type, parser.arg1, parser.arg2)
+        code_writer.write_push_pop(parser.command_type, parser.arg1, parser.arg2)
       when :C_LABEL
-        @code_writer.write_label(parser.arg1)
+        code_writer.write_label(parser.arg1)
       when :C_IF
-        @code_writer.write_if(parser.arg1)
+        code_writer.write_if(parser.arg1)
       when :C_GOTO
-        @code_writer.write_goto(parser.arg1)
+        code_writer.write_goto(parser.arg1)
       when :C_ARITHMETIC
-        @code_writer.write_arithmetic(parser.arg1)
+        code_writer.write_arithmetic(parser.arg1)
       when :C_FUNCTION
-        @code_writer.write_function(parser.arg1, parser.arg2)
+        code_writer.write_function(parser.arg1, parser.arg2)
       when :C_RETURN
-        @code_writer.write_return
+        code_writer.write_return
       else
         warn("Unknown command type passed into VMTranslator")
       end
     end
 
-    @code_writer.close
-    @out.string
+    code_writer.close
+    out.string
   end
 end
